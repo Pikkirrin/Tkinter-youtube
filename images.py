0eb64b3033a8b10470a0162302aae3e9dbc85258
Victor Hugo González Hernández <grupotercero_b@hotmail.com>
2020-09-03 19:36:31 -0500

    Aqui empieza el tercer programa del curso
    frames

--- images.py
+++ images.py
@@ -14,6 +14,8 @@ my_img6 = ImageTk.PhotoImage(Image.open("images/Iphone_6.png"))
 
 image_list = [my_img1, my_img2, my_img3, my_img4, my_img5, my_img6]
 
+status = Label(win, text="Image 1 of " + str(len(image_list)), bd=1, relief=SUNKEN, anchor=E)
+
 my_label = Label(image=my_img1)
 my_label.grid(row=0, column=0, columnspan=3)
 
@@ -34,6 +36,9 @@ def forward(img_num):
     my_label.grid(row=0, column=0, columnspan=3)
     back_button.grid(row=1, column=0)
     forward_button.grid(row=1, column=2)
+    
+    status = Label(win, text="Image " + str(img_num) + " of " + str(len(image_list)), bd=1, relief=SUNKEN, anchor=E)
+    status.grid(row=2, column=0, columnspan=3, sticky=W + E)
 
 
 def back(img_num):
@@ -53,6 +58,9 @@ def back(img_num):
     back_button.grid(row=1, column=0)
     forward_button.grid(row=1, column=2)
 
+    status = Label(win, text="Image " + str(img_num) + " of " + str(len(image_list)), bd=1, relief=SUNKEN, anchor=E)
+    status.grid(row=2, column=0, columnspan=3, sticky=W + E)
+
 
 back_button = Button(win, text="<<", command=back, state=DISABLED)
 exit_button = Button(win, text="EXIT", command=win.quit)
@@ -60,6 +68,7 @@ forward_button = Button(win, text=">>", command=lambda: forward(2))
 
 back_button.grid(row=1, column=0)
 exit_button.grid(row=1, column=1)
-forward_button.grid(row=1, column=2)
+forward_button.grid(row=1, column=2, pady=10)
+status.grid(row=2, column=0, columnspan=3, sticky=W+E)
 
 win.mainloop()
