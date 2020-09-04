6e997f7fbbcbf08d9485ab7f9664be2233ff81fd
Victor Hugo González Hernández <grupotercero_b@hotmail.com>
2020-09-04 13:27:20 -0500

    Radio buttons

--- radio.py
+++ radio.py
@@ -5,13 +5,35 @@ root = Tk()
 root.title("radio buttons")
 root.iconbitmap("Head.ico")
 
-r = IntVar()
-r.set(2)
+# r = IntVar()
+# r.set("2")
 
-Radiobutton(root, text="Option 1", variable=r, value=1).pack()
-Radiobutton(root, text="Option 2", variable=r, value=2).pack()
+MODES = [
+    ("Pepperoni", "Pepperoni"),
+    ("Cheese", "Cheese"),
+    ("Mushroom", "Mushroom"),
+    ("Onion", "Onion"),
+]
 
-myLabel = Label(root, text=r.get())
+pizza = StringVar()
+pizza.set("Pepperoni")
+
+for text, mode in MODES:
+    Radiobutton(root, text=text, variable=pizza, value=mode).pack(anchor=W)
+
+
+def clicked(value):
+    myLabel = Label(root, text=value)
+    myLabel.pack()
+
+
+# Radiobutton(root, text="Option 1", variable=r, value=1, command=lambda: clicked(r.get())).pack()
+# Radiobutton(root, text="Option 2", variable=r, value=2, command=lambda: clicked(r.get())).pack()
+
+myLabel = Label(root, text=pizza.get())
 myLabel.pack()
+
+my_Button = Button(root, text="click me!", command=lambda: clicked(pizza.get()))
+my_Button.pack()
 
 root.mainloop()
