9599c796d78a28c369eac004d6bb2599a8c354f7
Victor Hugo González Hernández <grupotercero_b@hotmail.com>
2020-09-05 18:06:54 -0500

    Update

--- Databases.py
+++ Databases.py
@@ -26,6 +26,115 @@ c.execute("""CREATE TABLE addresses (
         )""")
 '''
 
+
+# Create a submit function for db
+def submit():
+    # Create a database or connect to one
+    conn = sqlite3.connect('address_book.db')
+
+    # Create a cursor
+    c = conn.cursor()
+
+    # Insert into table
+    c.execute("INSERT INTO addresses VALUES (:f_name, :l_name, :address, :city, :state, :zipcode)",
+              {
+                  'f_name': f_name.get(),
+                  'l_name': l_name.get(),
+                  'address': address.get(),
+                  'city': city.get(),
+                  'state': state.get(),
+                  'zipcode': zipcode.get()
+              })
+
+    # Commit Changes
+    conn.commit()
+
+    # Close Connection
+    conn.close()
+
+    # Clear the text boxes
+    f_name.delete(0, END)
+    l_name.delete(0, END)
+    address.delete(0, END)
+    city.delete(0, END)
+    state.delete(0, END)
+    zipcode.delete(0, END)
+
+
+# Create Query function
+def query():
+    # Create a database or connect to one
+    conn = sqlite3.connect('address_book.db')
+
+    # Create a cursor
+    c = conn.cursor()
+
+    # Query the db
+    c.execute("SELECT *, oid FROM addresses")
+    records = c.fetchall()
+    print(records)
+
+    # Loop thru results
+    print_records = ''
+    for record in records:
+        print_records += str(record[0]) + " " + str(record[1]) + "\n"
+
+    query_label = Label(root, text=print_records)
+    query_label.grid(row=8, column=0, columnspan=2)
+
+    # Commit Changes
+    conn.commit()
+
+    # Close Connection
+    conn.close()
+
+
+# Ponemos las entradas en la pantalla
+f_name = Entry(root, width=30)
+f_name.grid(row=0, column=1, padx=20)
+
+l_name = Entry(root, width=30)
+l_name.grid(row=1, column=1, padx=20)
+
+address = Entry(root, width=30)
+address.grid(row=2, column=1, padx=20)
+
+city = Entry(root, width=30)
+city.grid(row=3, column=1, padx=20)
+
+state = Entry(root, width=30)
+state.grid(row=4, column=1, padx=20)
+
+zipcode = Entry(root, width=30)
+zipcode.grid(row=5, column=1, padx=20)
+
+# Ahora ponemos las desripciones
+f_name_label = Label(root, text="First name")
+f_name_label.grid(row=0, column=0)
+
+l_name_label = Label(root, text="Last name")
+l_name_label.grid(row=1, column=0)
+
+address_label = Label(root, text="Address")
+address_label.grid(row=2, column=0)
+
+city_label = Label(root, text="City")
+city_label.grid(row=3, column=0)
+
+state_label = Label(root, text="State")
+state_label.grid(row=4, column=0)
+
+zipcode_label = Label(root, text="Zipcode")
+zipcode_label.grid(row=5, column=0)
+
+# Create a submit button
+submit_btn = Button(root, text="Add record to db", command=submit)
+submit_btn.grid(row=6, column=0, columnspan=2, pady=10, padx=10, ipadx=137)
+
+# Create a Query button
+query_btn = Button(root, text="Show records", command=query)
+query_btn.grid(row=7, column=0, columnspan=2, pady=10, padx=10, ipadx=137)
+
 # Commit Changes
 conn.commit()
 
